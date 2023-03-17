2f767b563ab8288304e72684b0a73b55b8b687bc
Hugo Moroyoqui <hugo_moroyoqui@hotmail.com>
2023-03-17 12:54:04 -0700

    Update test.js

--- test.js
+++ test.js
@@ -11,3 +11,12 @@ const instancia = new Clase('Hugo');
 //console.log(instancia);
 console.log(instancia.nombre);
 console.log(instancia.calculo(15));
+
+fetch("https://app-mykey.herokuapp.com/api/brands")
+.then((response) => {
+  return response.json();
+})
+.then((data) => {
+  console.log(data);
+})
+
